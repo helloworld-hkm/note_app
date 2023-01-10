ee21a06cbaee0c0d318f2b1ed1b3df625aefe10f
helloworld-hkm <81341249+helloworld-hkm@users.noreply.github.com>
2023-01-10 18:06:25 +0700

    update akhir

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ if(window.openDatabase){
   var mydb = openDatabase("noteapp", '1.0', "WebSQL Database", 2 * 1024 * 1024);
   mydb.transaction(function(tx){
 		tx.executeSql("create table if not exists user(username varchar(25) primary key, password varchar(25))");
-    tx.executeSql("insert into user values ('note', 'noteapp')");
+    tx.executeSql("insert into user values ('fasya', 'noteapp')");
     tx.executeSql("create table if not exists note (judul varchar(25) primary key, kategori varchar(25), catatan varchar(255), tanggal date, is_bookmark varchar(10))");
-		tx.executeSql("insert into note values ('hakim', 'noteapp','catatan tentang','2008-11-11','true')");
+		tx.executeSql("insert into note values ('fasya', 'kuliah','tugas mobile membuat aplikasi pencatatan sederhana dengan onsen ui','11-1-2023','true')");
     
   });
 }else{
@@ -18,7 +18,6 @@ function tampil_data(){
     tx.executeSql("select * from note",[], ambil_data);
   });    
 }
-
 function ambil_data(transaction, results){
   var list_holder=document.getElementById("list-data");
   list_holder.innerHTML="";
@@ -49,31 +48,75 @@ function ambil_data_personal(transaction, results){
   }
 }
 
-function ambil_detail(transaction, results){
-  // var list_holder=document.getElementById("list-detail");
-  // list_holder.innerHTML="";
-  var judul=document.getElementById("detail_judul");
-  judul.innerHTML="";
-  var tanggal=document.getElementById("detail_tanggal");
-  tanggal.innerHTML="";
-  var kategori=document.getElementById("detail_kategori");
-  kategori.innerHTML="";
-  var catatan=document.getElementById("detail_catatan");
-  // catatan.innerHTML="";
+// tampil data untuk kategori hiburan
+function tampil_data_hiburan(){
+  mydb.transaction(function(tx){
+    tx.executeSql("select * from note where kategori = 'hiburan'",[], ambil_data_hiburan);
+  });    
+}
+
+function ambil_data_hiburan(transaction, results){
+  var list_holder=document.getElementById("list-data-hiburan");
+  list_holder.innerHTML="";
+
   var i;
   for(i=0; i<results.rows.length;i++){
     var nomor=i+1;
     var row=results.rows.item(i);
-    judul.innerHTML=row.judul;
-    tanggal.innerHTML=row.tanggal;
-    kategori.innerHTML=row.kategori;
-    catatan.innerHTML=row.catatan;
-    // list_holder.innerHTML="  <ons-list-item>"+row.judul+"</ons-list-item>";
-		// list_holder.innerHTML+="<ons-list-item><div class='left'  >"+ nomor +"</div><div class='center' style='font-weight:bold'onclick='detail(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\")'>"+ row.judul +"</div><div class='right'><a onclick='edit_data(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\");'><ons-icon icon='fa-edit'></ons-icon></a>&emsp;<a onclick='hapus_data(\""+row.judul+"\");'><ons-icon icon='fa-trash'></ons-icon></a></div></ons-list-item>";    
+		list_holder.innerHTML+="<ons-list-item><div class='left'  >"+ nomor +"</div><div class='center' style='font-weight:bold'onclick='detail(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\")'>"+ row.judul +"</div><div class='right'><a onclick='edit_data(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\");'><ons-icon icon='fa-edit'></ons-icon></a>&emsp;<a onclick='hapus_data(\""+row.judul+"\");'><ons-icon icon='fa-trash'></ons-icon></a></div></ons-list-item>";    
   }
 }
-function cek() {
+
+// tampil data untuk kuliah
+function tampil_data_kuliah(){
+  mydb.transaction(function(tx){
+    tx.executeSql("select * from note where kategori = 'kuliah'",[], ambil_data_kuliah);
+  });    
+}
+
+function ambil_data_kuliah(transaction, results){
+  var list_holder=document.getElementById("list-data-kuliah");
+  list_holder.innerHTML="";
+
+  var i;
+  for(i=0; i<results.rows.length;i++){
+    var nomor=i+1;
+    var row=results.rows.item(i);
+		list_holder.innerHTML+="<ons-list-item><div class='left'  >"+ nomor +"</div><div class='center' style='font-weight:bold'onclick='detail(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\")'>"+ row.judul +"</div><div class='right'><a onclick='edit_data(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\");'><ons-icon icon='fa-edit'></ons-icon></a>&emsp;<a onclick='hapus_data(\""+row.judul+"\");'><ons-icon icon='fa-trash'></ons-icon></a></div></ons-list-item>";    
+  }
+}
+
+
+// tampil data untuk bookmark
+function tampil_data_bookmark(){
+  mydb.transaction(function(tx){
+    tx.executeSql("select * from note where is_bookmark = 'true'",[], ambil_data_bookmark);
+  });    
+}
+
+function ambil_data_bookmark(transaction, results){
+  var list_holder=document.getElementById("list-data-bookmark");
+  list_holder.innerHTML="";
+
+  var i;
+  for(i=0; i<results.rows.length;i++){
+    var nomor=i+1;
+    var row=results.rows.item(i);
+		list_holder.innerHTML+="<ons-list-item><div class='left'  >"+ nomor +"</div><div class='center' style='font-weight:bold'onclick='detail(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\")'>"+ row.judul +"</div><div class='right'><a onclick='edit_data(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\");'><ons-icon icon='fa-edit'></ons-icon></a>&emsp;<a onclick='hapus_data(\""+row.judul+"\");'><ons-icon icon='fa-trash'></ons-icon></a></div></ons-list-item>";    
+  }
+}
+
+
+function bookmark() {
   
+  var judul = document.getElementById('detail_judul').textContent;
+console.log(judul);
+  mydb.transaction(function (tx) {
+    tx.executeSql("update note set is_bookmark ='true' WHERE judul = ? ", [judul], tampil_data);    
+  });
+  alert("berhasil ditambahkan ke bookmark");
+  window.location.reload();
+
 }
 function tambah_data() {
   var judul = document.getElementById('judul').value;
@@ -96,7 +139,7 @@ function tambah_data() {
   
  
 }
-function detail(judul,kategori,catatan,tanggal,is_bookmark){
+function detail(judul){
   // console.log(judul);
   let data_judul=judul;
   mydb.transaction(function(tx){
@@ -137,13 +180,92 @@ fn.load('detail.html')
   // document.getElementById("jumlah").value=jumlah;
   // document.getElementById("simpan").innerHTML="<ons-button style=\"background-color: orange\" onclick='ubah_data();'>Update</a>";
 }
+function ambil_detail(transaction, results){
+  // var list_holder=document.getElementById("list-detail");
+  // list_holder.innerHTML="";
+  var judul=document.getElementById("detail_judul");
+  judul.innerHTML="";
+  var tanggal=document.getElementById("detail_tanggal");
+  tanggal.innerHTML="";
+  var kategori=document.getElementById("detail_kategori");
+  kategori.innerHTML="";
+  var catatan=document.getElementById("detail_catatan");
+  // catatan.innerHTML="";
+  var i;
+  for(i=0; i<results.rows.length;i++){
+    var nomor=i+1;
+    var row=results.rows.item(i);
+    judul.innerHTML=row.judul;
+    tanggal.innerHTML=row.tanggal;
+    kategori.innerHTML=row.kategori;
+    catatan.innerHTML=row.catatan;
+    // list_holder.innerHTML="  <ons-list-item>"+row.judul+"</ons-list-item>";
+		// list_holder.innerHTML+="<ons-list-item><div class='left'  >"+ nomor +"</div><div class='center' style='font-weight:bold'onclick='detail(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\")'>"+ row.judul +"</div><div class='right'><a onclick='edit_data(\""+row.judul+"\",\""+row.kategori+"\",\""+row.catatan+"\",\""+row.tanggal+"\",\""+row.is_bookmark+"\");'><ons-icon icon='fa-edit'></ons-icon></a>&emsp;<a onclick='hapus_data(\""+row.judul+"\");'><ons-icon icon='fa-trash'></ons-icon></a></div></ons-list-item>";    
+  }
+}
+// funsi edit
+function edit_data(judul){
+  // console.log(judul);
+  let data_judul=judul;
+  mydb.transaction(function(tx){
+    tx.executeSql("select * from note where judul=?",[judul], form_edit);
+  });    
+fn.load('edit.html')
+
+}
+function form_edit(transaction, results){
+  // var list_holder=document.getElementById("list-detail");
+  // list_holder.innerHTML="";
+  var judul=document.getElementById("judul");
+  var id=document.getElementById("id");
+  judul.value="";
+  var kategori=document.getElementById("kategori");
+  kategori.value="";
+  var catatan=document.getElementById("catatan");
+  // catatan.innerHTML="";
+  var i;
+  for(i=0; i<results.rows.length;i++){
+    var nomor=i+1;
+    var row=results.rows.item(i);
+    judul.value=row.judul;
+    id.value=row.judul;
+    kategori.value=row.kategori;
+    catatan.value=row.catatan;
+  
+  }
+}
+
+function ubah_data() {
+  
+  var judul = document.getElementById('judul').value;
+  var id=document.getElementById('id').value;
+  var kategori = document.getElementById('kategori').value;
+  var catatan = document.getElementById('catatan').value;
+  // console.log(judul);
+  mydb.transaction(function (tx) {
+    tx.executeSql("update note set judul =?, kategori =?, catatan =? WHERE judul = ?", [judul,kategori,catatan,id], tampil_data);    
+  });
+	alert("data berhasil diubah");
+  // window.location.reload();
+  fn.load("home.html");
+}
 
 window.fn = {};
 sessionStorage.setItem("login", "fasya");
 let user = sessionStorage.getItem("login")
 
 document.getElementById("user").innerHTML = user;
-
+function hapus_data(judul){
+  var konfirmasi = confirm("Anda yakin akan menghapus data " + judul + " ? ");
+  if(konfirmasi){
+    mydb.transaction(function(t){
+			t.executeSql("delete from note where judul = ?", [judul], tampil_data);
+    });		
+  }
+	alert("data berhasil dihapus");
+  window.location.reload();
+  fn.load("home.html");
+}
 
 window.fn.open = function() {
   var menu = document.getElementById('menu');
@@ -151,6 +273,9 @@ window.fn.open = function() {
 };
 
 window.fn.load = function(page) {
+  tampil_data_hiburan();
+  tampil_data_bookmark();
+  tampil_data_kuliah();
   tampil_data_personal();
   tampil_data();
   var content = document.getElementById('content');
